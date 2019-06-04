ecfb7fea74057e77cc6592d6cf4094be7b177378
Tyler Vegh <tyler@vegh.tv>
2019-06-04 02:02:38 -0400

    First Version

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+using RoR2;
+using System.Linq;
+
+
+namespace IncreaseHuntressRange
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.TeaBoneJones.IncreaseHuntressRange", "Increase Huntress Range", "1.0.0")]
+    public class IncreaseHuntressRange : BaseUnityPlugin
+    {
+        private static ConfigWrapper<bool> godMode;
+        private static ConfigWrapper<float> scaleFactor;
+        public void Awake()
+        {
+            godMode = Config.Wrap<bool>("Huntress", "GodMode", "Set the range to be so massive that you can attack any enemy that is visible", false);
+            scaleFactor = Config.Wrap<float>("Huntress", "ScaleFactor", "Set how much the range increases (in meters) each level. Recommended: 3 (but I'm not your dad)", 3f);
+            On.RoR2.HuntressTracker.SearchForTarget += (orig, self, aimRay) =>
+            {
+                float distance;
+                if (godMode.Value)
+                {
+                    distance = 10000f;
+                }
+                else
+                {
+                    // get current level
+                    uint currentLevel = TeamManager.instance.GetTeamLevel(TeamIndex.Player);
+                    // change distance based on level.
+                    // level 1 = 60 (default)
+                    // level 40 = 177
+                    distance = 60f + (scaleFactor.Value * (currentLevel - 1));
+                }
+
+                self.GetFieldValue<BullseyeSearch>("search").teamMaskFilter = TeamMask.all;
+                self.GetFieldValue<BullseyeSearch>("search").teamMaskFilter.RemoveTeam(self.GetFieldValue<TeamComponent>("teamComponent").teamIndex);
+                self.GetFieldValue<BullseyeSearch>("search").filterByLoS = true;
+                self.GetFieldValue<BullseyeSearch>("search").searchOrigin = aimRay.origin;
+                self.GetFieldValue<BullseyeSearch>("search").searchDirection = aimRay.direction;
+                self.GetFieldValue<BullseyeSearch>("search").sortMode = BullseyeSearch.SortMode.Distance;
+
+                self.GetFieldValue<BullseyeSearch>("search").maxDistanceFilter = distance;
+
+                self.GetFieldValue<BullseyeSearch>("search").maxAngleFilter = self.maxTrackingAngle;
+                self.GetFieldValue<BullseyeSearch>("search").RefreshCandidates();
+                self.SetFieldValue<HurtBox>("trackingTarget", self.GetFieldValue<BullseyeSearch>("search").GetResults().FirstOrDefault<HurtBox>());
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Increase Huntress Range
+
+Increase the range of Huntress every time you level up. Or turn on God Mode and increase the range to encompass the whole map.
+
+## Config
+
+God Mode: Set Huntress range to 10,000 meters. Encompasses the whole map, so any enemy that is visible can be shot at.
+Scale Factor: Set how much the range increases (in meters) each level. Recommended: 3 (but I'm not your dad) 
+
+## Known Issues
+
+None so far. But if you find any, create a new issue on the github:
+https://github.com/tvegh/IncreaseHuntressRange/issues
+
+## Changelog
+
+- 1.0.0: First version. I'm sure there will be no others, because my code is always flawless.
